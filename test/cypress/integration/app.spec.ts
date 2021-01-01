e9bf9177fa7a14e9ecd707980531798f6dfccc50
aboyce <adam@aboyce.uk>
2021-01-01 16:51:42 +0000

    Added first cypress test.

--- test/cypress/integration/app.spec.ts
+++ test/cypress/integration/app.spec.ts
@@ -1,5 +1,16 @@
 describe('App loads as expected', () => {
-  it('displays the flex and grid packages', () => {
-    cy.visit('http://localhost:40000')
+  it('displays the flex, grid and home buttons', () => {
+    cy.visit('/')
+
+    cy.contains('Home')
+
+    cy.contains('@react-css/flex').click()
+    cy.url().should('include', '/flex')
+
+    cy.contains('@react-css/grid').click()
+    cy.url().should('include', '/grid')
+
+    cy.contains('Home').click()
+    cy.url().should('include', '/')
   })
 })
