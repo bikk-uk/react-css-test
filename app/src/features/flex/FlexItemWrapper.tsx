4d30699cfe6112e42feee6b4cde42aa06ade0f8e
aboyce <adam@aboyce.uk>
2021-01-02 17:04:13 +0000

    Updated and minimised use of test ids.

--- app/src/features/flex/FlexItemWrapper.tsx
+++ app/src/features/flex/FlexItemWrapper.tsx
@@ -5,9 +5,8 @@ import Flex from '@react-css/flex'
 // Components
 import Header from '../helpers/Header'
 
-const Container = ({ id, children }: { id: string; children: React.ReactElement | React.ReactElement[] }) => (
+const Container = ({ children }: { children: React.ReactElement | React.ReactElement[] }) => (
   <div
-    id={id}
     style={{
       height: '5em',
       width: '100%',
@@ -22,88 +21,88 @@ const Container = ({ id, children }: { id: string; children: React.ReactElement 
 export function FlexItemWrapper(): React.ReactElement {
   return (
     <Flex column>
-      <Container id='flex-item-default-container'>
+      <Container>
         <Header title='Flex Item Default' />
-        <Flex id='flex-item-default-container'>
-          <Flex.Item id='flex-item-default'></Flex.Item>
+        <Flex data-testid='flex-item-default-container'>
+          <Flex.Item data-testid='flex-item-default'></Flex.Item>
         </Flex>
       </Container>
-      <Container id='flex-item-order-container'>
+      <Container>
         <Header title='Flex Item - order' />
-        <Flex id='flex-item-order-container'>
-          <Flex.Item id='flex-item-order' order={5}></Flex.Item>
+        <Flex data-testid='flex-item-order-container'>
+          <Flex.Item data-testid='flex-item-order' order={5}></Flex.Item>
         </Flex>
       </Container>
-      <Container id='flex-item-grow-container'>
+      <Container>
         <Header title='Flex Item - grow' />
-        <Flex id='flex-item-grow-container'>
-          <Flex.Item id='flex-item-grow' grow={2}></Flex.Item>
+        <Flex data-testid='flex-item-grow-container'>
+          <Flex.Item data-testid='flex-item-grow' grow={2}></Flex.Item>
         </Flex>
       </Container>
-      <Container id='flex-item-shrink-container'>
+      <Container>
         <Header title='Flex Item - shrink' />
-        <Flex id='flex-item-shrink-container'>
-          <Flex.Item id='flex-item-shrink' shrink={3}></Flex.Item>
+        <Flex data-testid='flex-item-shrink-container'>
+          <Flex.Item data-testid='flex-item-shrink' shrink={3}></Flex.Item>
         </Flex>
       </Container>
-      <Container id='flex-item-basis-container'>
+      <Container>
         <Header title='Flex Item - basis' />
-        <Flex id='flex-item-basis-container'>
-          <Flex.Item id='flex-item-basis' basis='revert'></Flex.Item>
+        <Flex data-testid='flex-item-basis-container'>
+          <Flex.Item data-testid='flex-item-basis' basis='revert'></Flex.Item>
         </Flex>
       </Container>
-      <Container id='flex-item-flex-container'>
+      <Container>
         <Header title='Flex Item - flex' />
-        <Flex id='flex-item-flex-container'>
-          <Flex.Item id='flex-item-flex' flex='none'></Flex.Item>
+        <Flex data-testid='flex-item-flex-container'>
+          <Flex.Item data-testid='flex-item-flex' flex='none'></Flex.Item>
         </Flex>
       </Container>
-      <Container id='flex-item-alignSelf-container'>
+      <Container>
         <Header title='Flex Item - alignSelf' />
-        <Flex id='flex-item-alignSelf-container'>
-          <Flex.Item id='flex-item-alignSelf' alignSelf='flex-start'></Flex.Item>
+        <Flex data-testid='flex-item-alignSelf-container'>
+          <Flex.Item data-testid='flex-item-alignSelf' alignSelf='flex-start'></Flex.Item>
         </Flex>
       </Container>
-      <Container id='flex-item-alignSelfAuto-container'>
+      <Container>
         <Header title='Flex Item - alignSelfAuto' />
-        <Flex id='flex-item-alignSelfAuto-container'>
-          <Flex.Item id='flex-item-alignSelfAuto' alignSelfAuto></Flex.Item>
+        <Flex data-testid='flex-item-alignSelfAuto-container'>
+          <Flex.Item data-testid='flex-item-alignSelfAuto' alignSelfAuto></Flex.Item>
         </Flex>
       </Container>
-      <Container id='flex-item-alignSelfStart-container'>
+      <Container>
         <Header title='Flex Item - alignSelfStart' />
-        <Flex id='flex-item-alignSelfStart-container'>
-          <Flex.Item id='flex-item-alignSelfStart' alignSelfStart></Flex.Item>
+        <Flex data-testid='flex-item-alignSelfStart-container'>
+          <Flex.Item data-testid='flex-item-alignSelfStart' alignSelfStart></Flex.Item>
         </Flex>
       </Container>
-      <Container id='flex-item-alignSelfEnd-container'>
+      <Container>
         <Header title='Flex Item - alignSelfEnd' />
-        <Flex id='flex-item-alignSelfEnd-container'>
-          <Flex.Item id='flex-item-alignSelfEnd' alignSelfEnd></Flex.Item>
+        <Flex data-testid='flex-item-alignSelfEnd-container'>
+          <Flex.Item data-testid='flex-item-alignSelfEnd' alignSelfEnd></Flex.Item>
         </Flex>
       </Container>
-      <Container id='flex-item-alignSelfCenter-container'>
+      <Container>
         <Header title='Flex Item - alignSelfCenter' />
-        <Flex id='flex-item-alignSelfCenter-container'>
-          <Flex.Item id='flex-item-alignSelfCenter' alignSelfCenter></Flex.Item>
+        <Flex data-testid='flex-item-alignSelfCenter-container'>
+          <Flex.Item data-testid='flex-item-alignSelfCenter' alignSelfCenter></Flex.Item>
         </Flex>
       </Container>
-      <Container id='flex-item-alignSelfBaseline-container'>
+      <Container>
         <Header title='Flex Item - alignSelfBaseline' />
-        <Flex id='flex-item-alignSelfBaseline-container'>
-          <Flex.Item id='flex-item-alignSelfBaseline' alignSelfBaseline></Flex.Item>
+        <Flex data-testid='flex-item-alignSelfBaseline-container'>
+          <Flex.Item data-testid='flex-item-alignSelfBaseline' alignSelfBaseline></Flex.Item>
         </Flex>
       </Container>
-      <Container id='flex-item-alignSelfStretch-container'>
+      <Container>
         <Header title='Flex Item - alignSelfStretch' />
-        <Flex id='flex-item-alignSelfStretch-container'>
-          <Flex.Item id='flex-item-alignSelfStretch' alignSelfStretch></Flex.Item>
+        <Flex data-testid='flex-item-alignSelfStretch-container'>
+          <Flex.Item data-testid='flex-item-alignSelfStretch' alignSelfStretch></Flex.Item>
         </Flex>
       </Container>
-      <Container id='flex-item-alignSelf-warning-container'>
+      <Container>
         <Header title='Flex Item - alignSelf Warning' />
-        <Flex id='flex-item-alignSelf-warning-container'>
-          <Flex.Item id='flex-item-alignSelf-warning' alignSelfStretch alignSelfAuto></Flex.Item>
+        <Flex data-testid='flex-item-alignSelf-warning-container'>
+          <Flex.Item data-testid='flex-item-alignSelf-warning' alignSelfStretch alignSelfAuto></Flex.Item>
         </Flex>
       </Container>
     </Flex>
