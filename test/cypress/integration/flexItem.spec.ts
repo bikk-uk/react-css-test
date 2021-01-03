09362739fb18c098ba472704c492b45801eb7133
aboyce <adam@aboyce.uk>
2021-01-03 17:47:13 +0000

    Removed duplicate setup calls.

--- test/cypress/integration/flexItem.spec.ts
+++ test/cypress/integration/flexItem.spec.ts
@@ -3,113 +3,89 @@ import { goToFlexItem } from '../helpers/goTo'
 import hasFlexContainer from '../helpers/hasFlexContainer'
 
 describe('Flex Item', () => {
-  it('handles a default flex item', () => {
+  beforeEach(() => {
     goToFlexItem()
+  })
 
+  it('handles a default flex item', () => {
     hasFlexContainer('flex-item-default-container')
     const flexItem = '[data-testid="flex-item-default"]'
     cy.get(flexItem).should('not.have.css', 'display', 'flex')
   })
 
   it('handles custom styles', () => {
-    goToFlexItem()
-
     hasFlexContainer('flex-item-custom-styles-container')
     const flexItem = '[data-testid="flex-item-custom-styles"]'
     cy.get(flexItem).should('have.css', 'width', '1px').should('have.css', 'height', '2px')
   })
 
   it('handles the order property', () => {
-    goToFlexItem()
-
     hasFlexContainer('flex-item-order-container')
     const flexItem = '[data-testid="flex-item-order"]'
     cy.get(flexItem).should('have.css', 'order', '5')
   })
 
   it('handles the grow property', () => {
-    goToFlexItem()
-
     hasFlexContainer('flex-item-grow-container')
     const flexItem = '[data-testid="flex-item-grow"]'
     cy.get(flexItem).should('have.css', 'flex-grow', '2')
   })
 
   it('handles the shrink property', () => {
-    goToFlexItem()
-
     hasFlexContainer('flex-item-shrink-container')
     const flexItem = '[data-testid="flex-item-shrink"]'
     cy.get(flexItem).should('have.css', 'flex-shrink', '3')
   })
 
   it('handles the basis property', () => {
-    goToFlexItem()
-
     hasFlexContainer('flex-item-basis-container')
     const flexItem = '[data-testid="flex-item-basis"]'
     cy.get(flexItem).should('have.css', 'flex-basis', '0px')
   })
 
   it('handles the flex property', () => {
-    goToFlexItem()
-
     hasFlexContainer('flex-item-flex-container')
     const flexItem = '[data-testid="flex-item-flex"]'
     cy.get(flexItem).should('have.css', 'flex', '1 2 5px')
   })
 
   it('handles the align-self property', () => {
-    goToFlexItem()
-
     hasFlexContainer('flex-item-alignSelf-container')
     const flexItem = '[data-testid="flex-item-alignSelf"]'
     cy.get(flexItem).should('have.css', 'align-self', 'flex-start')
   })
 
   it('handles the align-self auto shortcut property', () => {
-    goToFlexItem()
-
     hasFlexContainer('flex-item-alignSelfAuto-container')
     const flexItem = '[data-testid="flex-item-alignSelfAuto"]'
     cy.get(flexItem).should('have.css', 'align-self', 'auto')
   })
 
   it('handles the align-self start shortcut property', () => {
-    goToFlexItem()
-
     hasFlexContainer('flex-item-alignSelfStart-container')
     const flexItem = '[data-testid="flex-item-alignSelfStart"]'
     cy.get(flexItem).should('have.css', 'align-self', 'flex-start')
   })
 
   it('handles the align-self end shortcut property', () => {
-    goToFlexItem()
-
     hasFlexContainer('flex-item-alignSelfEnd-container')
     const flexItem = '[data-testid="flex-item-alignSelfEnd"]'
     cy.get(flexItem).should('have.css', 'align-self', 'flex-end')
   })
 
   it('handles the align-self center shortcut property', () => {
-    goToFlexItem()
-
     hasFlexContainer('flex-item-alignSelfCenter-container')
     const flexItem = '[data-testid="flex-item-alignSelfCenter"]'
     cy.get(flexItem).should('have.css', 'align-self', 'center')
   })
 
   it('handles the align-self baseline shortcut property', () => {
-    goToFlexItem()
-
     hasFlexContainer('flex-item-alignSelfBaseline-container')
     const flexItem = '[data-testid="flex-item-alignSelfBaseline"]'
     cy.get(flexItem).should('have.css', 'align-self', 'baseline')
   })
 
   it('handles the align-self stretch shortcut property', () => {
-    goToFlexItem()
-
     hasFlexContainer('flex-item-alignSelfStretch-container')
     const flexItem = '[data-testid="flex-item-alignSelfStretch"]'
     cy.get(flexItem).should('have.css', 'align-self', 'stretch')
