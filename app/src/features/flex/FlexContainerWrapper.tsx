89516870677064cee74631ed6a4c4d20d9fd65b5
aboyce <adam@aboyce.uk>
2021-01-10 18:12:05 +0000

    Added flex-direction tests for flex container.

--- app/src/features/flex/FlexContainerWrapper.tsx
+++ app/src/features/flex/FlexContainerWrapper.tsx
@@ -10,20 +10,50 @@ export default function FlexContainerWrapper(): React.ReactElement {
   return (
     <Flex column>
       <Container>
-        <Header title='Flex Container - Default' />
+        <Header title='Default' />
         <Flex data-testid='flex-container-default'>
           <Flex.Item></Flex.Item>
         </Flex>
       </Container>
       <Container>
-        <Header title='Flex Container - Inline' />
+        <Header title='Custom Styles' />
+        <Flex data-testid='flex-container-custom-styles' style={{ width: 3, height: 4 }}>
+          <Flex.Item></Flex.Item>
+        </Flex>
+      </Container>
+      <Container>
+        <Header code title='inline' />
         <Flex data-testid='flex-container-inline' inline>
           <Flex.Item></Flex.Item>
         </Flex>
       </Container>
       <Container>
-        <Header title='Flex Container - Custom Styles' />
-        <Flex data-testid='flex-container-custom-styles' style={{ width: 3, height: 4 }}>
+        <Header code title='flexDirection' />
+        <Flex data-testid='flex-container-flexDirection' flexDirection='row-reverse'>
+          <Flex.Item></Flex.Item>
+        </Flex>
+      </Container>
+      <Container>
+        <Header code title='flexDirection: row' />
+        <Flex data-testid='flex-container-flexDirectionRow' row>
+          <Flex.Item></Flex.Item>
+        </Flex>
+      </Container>
+      <Container>
+        <Header code title='flexDirection: row-reverse' />
+        <Flex data-testid='flex-container-flexDirectionRowReverse' rowReverse>
+          <Flex.Item></Flex.Item>
+        </Flex>
+      </Container>
+      <Container>
+        <Header code title='flexDirection: column' />
+        <Flex data-testid='flex-container-flexDirectionColumn' column>
+          <Flex.Item></Flex.Item>
+        </Flex>
+      </Container>
+      <Container>
+        <Header code title='flexDirection: column-reverse' />
+        <Flex data-testid='flex-container-flexDirectionColumnReverse' columnReverse>
           <Flex.Item></Flex.Item>
         </Flex>
       </Container>
--- test/cypress/integration/flexContainer.spec.ts
+++ test/cypress/integration/flexContainer.spec.ts
@@ -11,13 +11,37 @@ describe('Flex Container', () => {
     isFlexContainer('flex-container-default')
   })
 
-  it('handles an inline flex container', () => {
-    cy.get('[data-testid="flex-container-inline"]').should('have.css', 'display', 'inline-flex')
-  })
-
   it('handles custom styles', () => {
     isFlexContainerAnd('flex-container-custom-styles')
       .should('have.css', 'width', '3px')
       .should('have.css', 'height', '4px')
+  })
+
+  it('handles an inline flex container', () => {
+    cy.get('[data-testid="flex-container-inline"]').should('have.css', 'display', 'inline-flex')
+  })
+
+  it('handles the flex-direction property', () => {
+    isFlexContainerAnd('flex-container-flexDirection').should('have.css', 'flex-direction', 'row-reverse')
+  })
+
+  it('handles the flex-direction row shortcut property', () => {
+    isFlexContainerAnd('flex-container-flexDirectionRow').should('have.css', 'flex-direction', 'row')
+  })
+
+  it('handles the flex-direction row-reverse shortcut property', () => {
+    isFlexContainerAnd('flex-container-flexDirectionRowReverse').should('have.css', 'flex-direction', 'row-reverse')
+  })
+
+  it('handles the flex-direction column shortcut property', () => {
+    isFlexContainerAnd('flex-container-flexDirectionColumn').should('have.css', 'flex-direction', 'column')
+  })
+
+  it('handles the flex-direction column-reverse shortcut property', () => {
+    isFlexContainerAnd('flex-container-flexDirectionColumnReverse').should(
+      'have.css',
+      'flex-direction',
+      'column-reverse',
+    )
   })
 })
