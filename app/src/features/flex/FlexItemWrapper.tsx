701103add1155806c8df1db5bc57a7fe95acb68c
aboyce <adam@aboyce.uk>
2021-01-10 18:10:44 +0000

    Updated flex item headers.

--- app/src/features/flex/FlexItemWrapper.tsx
+++ app/src/features/flex/FlexItemWrapper.tsx
@@ -10,13 +10,13 @@ export default function FlexItemWrapper(): React.ReactElement {
   return (
     <Flex column>
       <Container>
-        <Header title='Flex Item - Default' />
+        <Header title='Default' />
         <Flex data-testid='flex-item-default-container'>
           <Flex.Item data-testid='flex-item-default'></Flex.Item>
         </Flex>
       </Container>
       <Container>
-        <Header title='Flex Item - Custom Styles' />
+        <Header title='Custom Styles' />
         <Flex data-testid='flex-item-custom-styles-container'>
           <Flex.Item
             data-testid='flex-item-custom-styles'
@@ -27,79 +27,79 @@ export default function FlexItemWrapper(): React.ReactElement {
         </Flex>
       </Container>
       <Container>
-        <Header title='Flex Item - order' />
+        <Header code title='order' />
         <Flex data-testid='flex-item-order-container'>
           <Flex.Item data-testid='flex-item-order' order={5}></Flex.Item>
         </Flex>
       </Container>
       <Container>
-        <Header title='Flex Item - grow' />
+        <Header code title='grow' />
         <Flex data-testid='flex-item-grow-container'>
           <Flex.Item data-testid='flex-item-grow' grow={2}></Flex.Item>
         </Flex>
       </Container>
       <Container>
-        <Header title='Flex Item - shrink' />
+        <Header code title='shrink' />
         <Flex data-testid='flex-item-shrink-container'>
           <Flex.Item data-testid='flex-item-shrink' shrink={3}></Flex.Item>
         </Flex>
       </Container>
       <Container>
-        <Header title='Flex Item - basis' />
+        <Header code title='basis' />
         <Flex data-testid='flex-item-basis-container'>
           <Flex.Item data-testid='flex-item-basis' basis={0}></Flex.Item>
         </Flex>
       </Container>
       <Container>
-        <Header title='Flex Item - flex' />
+        <Header code title='flex' />
         <Flex data-testid='flex-item-flex-container'>
           <Flex.Item data-testid='flex-item-flex' flex='1 2 5px'></Flex.Item>
         </Flex>
       </Container>
       <Container>
-        <Header title='Flex Item - alignSelf' />
+        <Header code title='alignSelf' />
         <Flex data-testid='flex-item-alignSelf-container'>
           <Flex.Item data-testid='flex-item-alignSelf' alignSelf='flex-start'></Flex.Item>
         </Flex>
       </Container>
       <Container>
-        <Header title='Flex Item - alignSelfAuto' />
+        <Header code title='alignSelf: auto' />
         <Flex data-testid='flex-item-alignSelfAuto-container'>
           <Flex.Item data-testid='flex-item-alignSelfAuto' alignSelfAuto></Flex.Item>
         </Flex>
       </Container>
       <Container>
-        <Header title='Flex Item - alignSelfStart' />
+        <Header code title='alignSelf: start' />
         <Flex data-testid='flex-item-alignSelfStart-container'>
           <Flex.Item data-testid='flex-item-alignSelfStart' alignSelfStart></Flex.Item>
         </Flex>
       </Container>
       <Container>
-        <Header title='Flex Item - alignSelfEnd' />
+        <Header code title='alignSelf: end' />
         <Flex data-testid='flex-item-alignSelfEnd-container'>
           <Flex.Item data-testid='flex-item-alignSelfEnd' alignSelfEnd></Flex.Item>
         </Flex>
       </Container>
       <Container>
-        <Header title='Flex Item - alignSelfCenter' />
+        <Header code title='alignSelf: center' />
         <Flex data-testid='flex-item-alignSelfCenter-container'>
           <Flex.Item data-testid='flex-item-alignSelfCenter' alignSelfCenter></Flex.Item>
         </Flex>
       </Container>
       <Container>
-        <Header title='Flex Item - alignSelfBaseline' />
+        <Header code title='alignSelf: baseline' />
         <Flex data-testid='flex-item-alignSelfBaseline-container'>
           <Flex.Item data-testid='flex-item-alignSelfBaseline' alignSelfBaseline></Flex.Item>
         </Flex>
       </Container>
       <Container>
-        <Header title='Flex Item - alignSelfStretch' />
+        <Header code title='alignSelf: stretch' />
         <Flex data-testid='flex-item-alignSelfStretch-container'>
           <Flex.Item data-testid='flex-item-alignSelfStretch' alignSelfStretch></Flex.Item>
         </Flex>
       </Container>
       <Container>
-        <Header title='Flex Item - alignSelf Warning' />
+        <Header title='alignSelf Warning' />
         <Flex data-testid='flex-item-alignSelf-warning-container'>
           <Flex.Item data-testid='flex-item-alignSelf-warning' alignSelfCenter alignSelfBaseline></Flex.Item>
         </Flex>
--- app/src/features/helpers/Header.tsx
+++ app/src/features/helpers/Header.tsx
@@ -3,8 +3,10 @@ import React from 'react'
 
 interface Props {
   title: string
+  code?: boolean
 }
 
-export default function Header({ title }: Props): React.ReactElement {
-  return <h5 style={{ marginBottom: 0, paddingTop: '0.5em' }}>{title}</h5>
+export default function Header({ title, code = false }: Props): React.ReactElement {
+  const content = code ? <code>{title}</code> : title
+  return <h5 style={{ marginBottom: 0, paddingTop: '0.5em' }}>{content}</h5>
 }
